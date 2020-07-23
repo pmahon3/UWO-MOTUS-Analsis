503a0dcc5ce3eb20cfae6b6d027923293d7aff53
Patrick Mahon <pmahon3@uwo.ca>
2020-07-23 16:42:08 -0400

    folder restruction; individual simulation refactoring

--- src/main/R/Individual/DataSimulation.R
+++ src/main/R/Individual/DataSimulation.R
@@ -1,38 +1,32 @@
-source("Inputs.R")
-
-data = list(size = CONSTANTS$nDays)
-
-
-ydat = matrix( nrow = CONSTANTS$nObservations, ncol = CONSTANTS$nDays)
-
-tdat = c(
-  seq(from = CONSTANTS$delta1Mu - 1, to = CONSTANTS$delta1Mu + 1, length.out = CONSTANTS$nObservations/2),
-  seq(from = CONSTANTS$delta2Mu - 1, to = CONSTANTS$delta2Mu + 1, length.out = CONSTANTS$nObservations/2)
-)
-
-
-for ( day in 1:CONSTANTS$nDays ){
-
-  for ( i in 1:CONSTANTS$nObservations){
-    if ( tdat[i] < CONSTANTS$delta1Mu ){
-      mu = CONSTANTS$m_y[1]
-      sd = CONSTANTS$sigmaMu[1]
+dataSimulation <- function(x, CONSTANTS, TRUEPARAMS){
+  data = list(size = CONSTANTS$nDays)
+  ydat = matrix( nrow = CONSTANTS$nObservations, ncol = CONSTANTS$nDays)
+  tdat = c(
+    seq(from = TRUEPARAMS$delta1 - 1, to = TRUEPARAMS$delta1 + 1, length.out = CONSTANTS$nObservations/2),
+    seq(from = TRUEPARAMS$delta2 - 1, to = TRUEPARAMS$delta2 + 1, length.out = CONSTANTS$nObservations/2)
+  )
+  for ( day in 1:CONSTANTS$nDays ){
+    for ( i in 1:CONSTANTS$nObservations){
+      if ( tdat[i] < TRUEPARAMS$delta1 ){
+        mu = TRUEPARAMS$muY[1]
+        sd = TRUEPARAMS$sigmaY[1]
+      }
+      else if ( day == TRUEPARAMS$nDays && tdat[i] < TRUEPARAMS$delta2 + TRUEPARAMS$delta ){
+        mu = TRUEPARAMS$muY[2]
+        sd = TRUEPARAMS$sigmaY[2]
+      }
+      else if( tdat[i] < TRUEPARAMS$delta2 ){
+        mu = TRUEPARAMS$muY[2]
+        sd = TRUEPARAMS$sigmaY[2]
+      }
+      else{
+        mu = TRUEPARAMS$muY[3]
+        sd = TRUEPARAMS$sigmaY[3]
+      }
+      ydat[i,day] = rnorm(1, mu, sd)
     }
-    else if ( day == CONSTANTS$nDays && tdat[i] < CONSTANTS$delta2Mu + DELTA ){
-      mu = CONSTANTS$m_y[2]
-      sd = CONSTANTS$sigmaMu[2]
-    }
-    else if( tdat[i] < CONSTANTS$delta2Mu ){
-      mu = CONSTANTS$m_y[2]
-      sd = CONSTANTS$sigmaMu[2]
-    }
-    else{
-      mu = CONSTANTS$m_y[3]
-      sd = CONSTANTS$sigmaMu[3]
-    }
-    ydat[i,day] = rnorm(1, mu, sd)
   }
+  DATA = c( list(y = ydat))
+  CONSTANTS = c(CONSTANTS, list(t = tdat))
+  return(list(data = DATA,constants = CONSTANTS))
 }
-
-DATA = c( list(y = ydat))
-CONSTANTS = c(CONSTANTS, list(t = tdat))
--- src/main/R/Individual/Inputs.R
+++ src/main/R/Individual/Inputs.R
@@ -1,5 +1,30 @@
 library(nimble)
-nimCode <- nimbleCode(
+trueParams = list(
+  delta = 1/4,
+  nDays = 10,
+  nObservations = 256,
+  muY = c(  one = -80, two = -50,  three = -80  ),
+  sigmaY = c(   one = 5,   two =  10,  three =  5  ),
+  delta1 = 6,
+  delta2 = 18,
+  delta = 1/4
+)
+
+constants = list(
+  nDays = 10,
+  nObservations = 256,
+  mu_y = c(  one = -80, two = -50,  three = -80  ),
+  sigmaMu = c(   one = 5,   two =  10,  three =  5  ),
+  delta1Mu = 6,
+  delta2Mu = 18,
+  sigmaDelta1 = 1,
+  sigmaDelta2 = 1,
+  ## Top level hyper parameter
+  deltaMu = 1/4,
+  sigmaDeltaMu = 1/120
+)
+
+modelCode <- nimbleCode(
   {
 
     ## DAY LIKELIHOODS
@@ -27,38 +52,8 @@ nimCode <- nimbleCode(
     delta ~ dnorm( deltaMu, 1 / sigmaDeltaMu^2 )
 
     for(k in 1:3){
-      mu[k] ~ dnorm( m_y[k], 1 / (sigmaMu[k]^2) )
+      mu[k] ~ dnorm( mu_y[k], 1 / (sigmaMu[k]^2) )
       tau[k] ~ dgamma(10, 2)
     }
   }
-)
-
-
-DELTA = 1/4
-
-CONSTANTS = list(
-
-  nDays = 10,
-
-  nObservations = 96,
-
-  interval = 24,
-
-  m_y     = c(  one = -80, two = -50,  three = -80  ),
-
-  sigmaMu = c(   one = 5,   two =  10,  three =  5  ),
-
-  delta1Mu = 6,
-
-  delta2Mu = 18,
-
-  sigmaDelta1 = 1,
-
-  sigmaDelta2 = 1,
-
-  ## Top level hyper parameter
-
-  deltaMu = 1/4,
-
-  sigmaDeltaMu = 1/120
 )
