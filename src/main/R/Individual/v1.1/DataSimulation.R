8612931983514f4a9867942a9caec719706fef31
Simon Bonner <sbonne6@uwo.ca>
2020-10-02 11:24:56 -0400

    Consolidated simulation of changepoints.

--- src/main/R/Individual/v1.1/DataSimulation.R
+++ src/main/R/Individual/v1.1/DataSimulation.R
@@ -2,37 +2,36 @@ dataSimulation <- function(x, CONSTANTS, TRUEPARAMS){
   ## Initialize data list
   data = list(size = CONSTANTS$nDays)
 
+  ## Generate parameters
+  deltas <- cbind(rnorm( CONSTANTS$nDays, TRUEPARAMS$muDelta1, TRUEPARAMS$sigmaDelta1),
+                  rnorm( CONSTANTS$nDays, TRUEPARAMS$muDelta2, TRUEPARAMS$sigmaDelta2))
+  
   ## Initialize storage matrices
   ydat = matrix( nrow = CONSTANTS$nDays, ncol = CONSTANTS$nObservations)
   tdat = matrix( nrow = CONSTANTS$nDays, ncol = CONSTANTS$nObservations)
-  deltas = matrix( nrow = CONSTANTS$nDays, ncol = 2)
 
   ## Loop over days
   for ( day in 1:CONSTANTS$nDays ){
-    ## Simulate changepoints
-    delta1 = rnorm( 1, TRUEPARAMS$muDelta1, TRUEPARAMS$sigmaDelta1)
-    delta2 = rnorm( 1, TRUEPARAMS$muDelta2, TRUEPARAMS$sigmaDelta2)
-
     ## Define observation times
     tdat[day,] = c(
-      seq(from = delta1 - 1, to = delta1 + 1, length.out = CONSTANTS$nObservations/2),
-      seq(from = delta2 - 1, to = delta2 + 1, length.out = CONSTANTS$nObservations/2)
+      seq(from = deltas[day,1] - 1, to = deltas[day,1] + 1, length.out = CONSTANTS$nObservations/2),
+      seq(from = deltas[day,2] - 1, to = deltas[day,2] + 1, length.out = CONSTANTS$nObservations/2)
     )
 
     ## Loop over observations within each day
     for ( j in 1:CONSTANTS$nObservations){
       ## Identify parameters of signal strength
-      if ( tdat[day,j] < delta1 ){
+      if ( tdat[day,j] < deltas[day,1] ){
         # Period 1
         mu = TRUEPARAMS$muY[1]
         sd = TRUEPARAMS$sigmaY[1]
       }
-      else if ( day == TRUEPARAMS$nDays && tdat[day,j] < delta2 + TRUEPARAMS$delta ){
+      else if ( day == TRUEPARAMS$nDays && tdat[day,j] < deltas[day,2] + TRUEPARAMS$delta ){
         ## Period 2 of final day
         mu = TRUEPARAMS$muY[2]
         sd = TRUEPARAMS$sigmaY[2]
       }
-      else if( tdat[day,j] < delta2 ){
+      else if( tdat[day,j] < deltas[day,2] ){
         ## Period 2 of other days
         mu = TRUEPARAMS$muY[2]
         sd = TRUEPARAMS$sigmaY[2]
@@ -47,9 +46,6 @@ dataSimulation <- function(x, CONSTANTS, TRUEPARAMS){
       ydat[day, j] = rnorm(1, mu, sd)
     }
 
-    ## Store deltas
-    deltas[day, 1] = delta1
-    deltas[day, 2] = delta2
   }
 
   ## Return output
