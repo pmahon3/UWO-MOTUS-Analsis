cc7a3bf23eb8aac2be66e366606dc8c905e937c7
Simon Bonner <sbonne6@uwo.ca>
2020-10-23 11:08:14 -0400

    Allowed mean signal strength to vary by day as well as period. Replaced inverse gamma priors with half-t priors.

--- src/main/R/Individual/v1.1/Inputs.R
+++ src/main/R/Individual/v1.1/Inputs.R
@@ -1,14 +1,18 @@
 library(nimble)
-trueParams = list(
-  nDays = 10,
-  nObservations = 600,
-  muY = c(  one = -80, two = -50,  three = -80  ),
-  sigmaY = c(   one = 5,   two =  10,  three =  5  ),
 
+nDays = 10
+
+trueParams = list(
+  nDays = nDays,
+  nObservations = 600,
+  muY = cbind(rnorm(nDays, -80, 5),
+              rnorm(nDays, -50, 5),
+              rnorm(nDays, -80, 5)),
+  sigmaY = c(5, 10, 5),
   muDelta1 = 6,
   muDelta2 = 18,
   sigmaDelta1 = .1,
-  sigmaDelta2 = .2,
+  sigmaDelta2 = .1,
 
   delta = 1/4
 )
@@ -18,11 +22,11 @@ constants = list(
   window1 = c(5,7),
   window2 = c(17,19),
   nObservations = 600,
-  mu_y = c(  one = -80, two = -50,  three = -80  ),
+  eta_y = c(  one = -80, two = -50,  three = -80  ),
   sigmaMu = c(   one = 10,   two =  10,  three =  10  ),
 
   muDelta = c(6, 18),
-  sigmaDelta = c(1, 1),
+  sigmaDelta = c(.25, .25),
 
   muDelta.prime = 0.25,
   sigmaMuDelta.prime = 0.5
@@ -38,7 +42,7 @@ modelCode <- nimbleCode(
           step( t[i,j] - delta[2,i] - delta.prime * step(i - nDays)) + 1
 
         ## Model response
-        y[i,j] ~ dnorm( mu[k[i,j]],tau[k[i,j]])
+        y[i,j] ~ dnorm( mu[i,k[i,j]],tau[k[i,j]])
       }
     }
 
@@ -53,13 +57,23 @@ modelCode <- nimbleCode(
     ## HYPERPRIORS
     for(k in 1:2){
       etaDelta[k] ~ dnorm(muDelta[k], 1/ sigmaDelta[k]^2)
-      tauDelta[k] ~ dgamma(.01,.01)
+      xiDelta[k] ~ T(dt(0, 50, 4),0,Inf)
+      tauDelta[k] <- 1/xiDelta[k]^2 
     }
     
     ## GLOBAL PRIORS
     for(k in 1:3){
-      mu[k] ~ dnorm( mu_y[k], 1 / (sigmaMu[k]^2) )
-      tau[k] ~ dgamma(.01, .01)
+      ## HYPERPRIORS
+      mu_y[k] ~ dnorm(eta_y[k], 1 / sigmaMu[k]^2 )
+      xi_y[k] ~ T(dt(0,5.5e-3,4),0,Inf)
+      tau_y[k] <- 1/xi_y[k]^2
+
+      for ( i in 1:nDays){
+        mu[i,k] ~ dnorm( mu_y[k], tau_y[k] )
+      }
+
+      xi[k] ~ T(dt(0,5.5e-3, 4), 0, )
+      tau[k] <- 1/xi[k]^2 
     }
   }
 )
