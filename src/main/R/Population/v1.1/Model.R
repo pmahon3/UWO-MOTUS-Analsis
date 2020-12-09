41bbda8743e4f0974c8fcbfae09668a20f68e598
Simon Bonner <sbonne6@uwo.ca>
2020-12-09 11:31:44 -0500

    Added variation by day (j) into the mean signal strength and by individual (i) and day (j) into the variance of the signal strength.

--- src/main/R/Population/v1.1/Model.R
+++ src/main/R/Population/v1.1/Model.R
@@ -8,7 +8,7 @@ modelCode <- nimbleCode(
           p[i,j,k] <- step( t[i,j,k] - delta[1,i,j] ) +
             step( t[i,j,k] - delta[2,i,j] - kappa[i] * step(i - nDays)) + 1
           ## Model response
-          y[i,j,k] ~ dnorm( muY[i,p[i,j,k]],tauY[p[i,j,k]])
+          y[i,j,k] ~ dnorm( muY[i,j,p[i,j,k]],tauY[i,j,p[i,j,k]])
         }
       }
     }
@@ -40,13 +40,16 @@ modelCode <- nimbleCode(
       mu_y[k] ~ dnorm(eta_y[k], 1 / sigmaMu[k]^2 )
       xi_y[k] ~ T(dt(0,tau_xi_y, df_xi_y),0,Inf)
       tau_y[k] <- 1/xi_y[k]^2
+
       
       for ( i in 1:nBirds){
-        muY[i,j,k] ~ dnorm( mu_y[k], tau_y[k] )
-      }
-      
-      xi[k] ~ T(dt(0,tau_xi, df_xi), 0, )
-      tauY[i,j,k] <- 1/xi[k]^2 
+        for(j in 1:nDays){
+          
+          muY[i,j,k] ~ dnorm( mu_y[k], tau_y[k] )
+          xi[i,j,k] ~ T(dt(0,tau_xi, df_xi), 0, )
+          
+          tauY[i,j,k] <- 1/xi[i,j,k]^2
+        }
     }
   }
 )
