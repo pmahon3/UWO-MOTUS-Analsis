6eb9070a6c4a2141154c35a9e3b9e88163ada786
Simon Bonner <sbonne6@uwo.ca>
2020-12-09 11:35:35 -0500

    Replaced k with p in the definition of the priors. This way k always indexes the observation and p (either by itself in the priors or indexed by i,j,k in the likelihood) indexes the period.

--- src/main/R/Population/v1.1/Model.R
+++ src/main/R/Population/v1.1/Model.R
@@ -16,8 +16,8 @@ modelCode <- nimbleCode(
     # PRIORS
     for ( i in 1:nBirds){
       for ( j in 1:nDays ){
-        for(k in 1:2){
-          delta[k,i,j] ~ dnorm( etaDelta[k,i], tauDelta[k,i] )
+        for(p in 1:2){
+          delta[p,i,j] ~ dnorm( etaDelta[p,i], tauDelta[p,i] )
         }
       }
       kappa[i] ~ dnorm( muKappa, 1 / sigmaKappa^2 )
@@ -25,30 +25,30 @@ modelCode <- nimbleCode(
    
     ## HYPERPRIORS
     for ( i in 1:nBirds){
-      for(k in 1:2){
-        etaDelta[k,i] ~ dnorm(muDelta[k], 1/ sigmaDelta[k]^2)
-        xiDelta[k,i] ~ T(dt(0, tau_xiDelta, df_xiDelta),0,Inf)
-        tauDelta[k,i] <- 1/xiDelta[k]^2 
+      for(p in 1:2){
+        etaDelta[p,i] ~ dnorm(muDelta[p], 1/ sigmaDelta[p]^2)
+        xiDelta[p,i] ~ T(dt(0, tau_xiDelta, df_xiDelta),0,Inf)
+        tauDelta[p,i] <- 1/xiDelta[p]^2 
       }
     }
     
     muKappa ~ dnorm( muMuKappa, 1 / sigmaMuKappa^2 )
     
     ## GLOBAL PRIORS
-    for(k in 1:3){
+    for(p in 1:3){
       ## HYPERPRIORS
-      mu_y[k] ~ dnorm(eta_y[k], 1 / sigmaMu[k]^2 )
-      xi_y[k] ~ T(dt(0,tau_xi_y, df_xi_y),0,Inf)
-      tau_y[k] <- 1/xi_y[k]^2
+      mu_y[p] ~ dnorm(eta_y[p], 1 / sigmaMu[p]^2 )
+      xi_y[p] ~ T(dt(0,tau_xi_y, df_xi_y),0,Inf)
+      tau_y[p] <- 1/xi_y[p]^2
 
       
       for ( i in 1:nBirds){
         for(j in 1:nDays){
           
-          muY[i,j,k] ~ dnorm( mu_y[k], tau_y[k] )
-          xi[i,j,k] ~ T(dt(0,tau_xi, df_xi), 0, )
+          muY[i,j,p] ~ dnorm( mu_y[p], tau_y[p] )
+          xi[i,j,p] ~ T(dt(0,tau_xi, df_xi), 0, )
           
-          tauY[i,j,k] <- 1/xi[i,j,k]^2
+          tauY[i,j,p] <- 1/xi[i,j,p]^2
         }
     }
   }
