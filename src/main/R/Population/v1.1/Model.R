9e1518e291be2dfca67097208db7f6999e2e2219
Simon Bonner <sbonner6@uwo.ca>
2021-06-15 13:44:58 -0400

    Restructuring model with changes of June 10.

--- src/main/R/Population/v1.1/Model.R
+++ src/main/R/Population/v1.1/Model.R
@@ -20,53 +20,76 @@ library(nimble)
 
 modelCode <- nimbleCode(
   {
-    # Likelihood
+    ##### Likelihood #####
+
+      ## 1) Signal strength
+
+      ## Observed values
     for ( i in 1:nBirds ){
       for ( j in 1:nDays ){
         for ( k in 1:nObservations){
           ## Identify period of day
           p[i,j,k] <- step( t[i,j,k] - delta[i,j,1] ) +
-            step( t[i,j,k] - delta[i,j,2] - delta.prime[i] * step(i - nDays)) + 1
+              step( t[i,j,k] - delta[i,j,2] - delta.prime[i] * step(i - nDays)) +
+              1
+            
           ## Model response
           y[i,j,k] ~ dnorm( muY[i,j,p[i,j,k]],tauY[i,j,p[i,j,k]])
         }
       }
     }
 
-    # delta.prime and muDeltaPrime priors
+      ## Period effects
+      for(p in 1:3){
+          for (bird in 1:nBirds){
+              for (day in 1:nDays){
+                  muY[bird,day,p] ~ dnorm( muMuY[p], tauMuY[p] )
+                  log(sigmaY[bird,day,p]) ~ dnorm(muSdY[p], tauSdY[p])
+              }
+          }
+      }
+
+ 
+    ## 2) Changepoints
+    # Final day effect
     for ( bird in 1:nBirds){
       delta.prime[bird] ~ dnorm( muDelta.prime, 1/sigmaDelta.prime^2 )
     }
-    muDelta.prime ~ dnorm( etaMuDelta.prime, 1/thetaMuDelta.prime^2 )
     
     # delta, muDelta, and muMuDelta priors
-    for(p in 1:2){
-      muMuDelta[p] ~ dnorm(etaMuMuDelta[p], 1/thetaMuMuDelta[p]^2)
       for ( bird in 1:nBirds){
-        muDelta[bird,p] ~ dnorm(muMuDelta[p], 1/thetaMuDelta[p]^2)
-        sigmaDelta[bird,p] ~ T(dt(0, sSigmaDelta, dfSigmaDelta),0,Inf)
-        tauDelta[bird,p] <- 1/sigmaDelta[bird,p]
-        for ( day in 1:nDays ){
-          delta[bird,day,p] ~ dnorm( muDelta[bird,p], tauDelta[bird,p] )
-        }
+          for(p in 1:2){
+              muDelta[bird,p] ~ dnorm(muMuDelta[p], 1/thetaMuDelta[p]^2)
+
+              for ( day in 1:nDays ){
+                  delta[bird,day,p] ~ dnorm( muDelta[bird,p], 1/sigmaDelta[p]^2)
+              }
+          }
       }
-    }
     
-    # muY, muMuY, sigmaY, and sigmaMuY priors
-    for(p in 1:3){
-      muMuMuY[p] ~ dnorm(etaY[p], 1 / sigmaEtaY[p]^2 )
-      sigmaMuMuY[p] ~ T(dt(0,sSigmaMuMuY[p], dfSigmaMuMuY[p]), 0, Inf)
-      tauMuMuY[p] <- 1/sigmaMuMuY[p]^2
-      for (bird in 1:nBirds){
-        muMuY[bird,p] ~ dnorm(muMuMuY[p], tauMuMuY[p])
-        sigmaMuY[bird,p] ~ T(dt(0,sSigmaMuY[p], dfSigmaMuY[p]), 0, Inf)
-        tauMuY[bird,p] <- 1/sigmaMuY[bird,p]^2
-        for (day in 1:nDays){
-          muY[bird,day,p] ~ dnorm( muMuY[bird,p], tauMuY[bird,p] )
-          sigmaY[bird,day,p] ~ T(dt(0, sSigmaY[p], dfSigmaY[p]), 0, Inf)
-          tauY[bird,day,p] <- 1/sigmaY[bird,day,p]^2
-        }
+   ##### Priors #####
+
+      ## Changepoints
+      muDelta.prime ~ dnorm( etaMuDelta.prime, 1/thetaMuDelta.prime^2 )
+
+      for(p in 1:2){
+          muMuDelta[p] ~ dnorm(etaMuMuDelta[p], 1/thetaMuMuDelta[p]^2)
+          sigmaDelta[p] <- T(dt(0,sSigmaDelta[p], dfSigmaDelta[p]), 0, Inf)
       }
-    }
+
+      ## Signal strength
+      for(p in 1:3){
+          ## Mean model
+          muMuY[p] ~ dnorm(etaMuY[p], 1 / sigmaMuY[p]^2 )
+          sigmaMuY[p] ~ T(dt(0,sSigmaMuY[p], dfSigmaMuY[p]), 0, Inf)
+          tauMuY[p] <- 1/sigmaMuY[p]^2
+
+          ## SD model
+          muSdY[p] ~ dnorm(etaSdY[p], 1/ sigmaSdY[p]^2)
+          sigmaSdY[p] ~ T(dt(0,sSigmaSdY[p], dfSigmaSdY[p]), 0, Inf)
+          tauSdY[p] <- 1/sigmaSdY[p]^2
+      }
   }
 )
+
+
