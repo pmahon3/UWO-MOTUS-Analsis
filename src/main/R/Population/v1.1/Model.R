7456614def20636c3bbf0b7c18967ae42590f30e
Simon Bonner <sbonner6@uwo.ca>
2021-06-15 19:36:05 -0400

    Fixed error in indexing with bird instead of day when identifying final day.

--- src/main/R/Population/v1.1/Model.R
+++ src/main/R/Population/v1.1/Model.R
@@ -29,7 +29,7 @@ modelCode <- nimbleCode(
         for ( k in 1:nObservations){
           ## Identify period of day
           p[i,j,k] <- step( t[i,j,k] - delta[i,j,1] ) +
-              step( t[i,j,k] - delta[i,j,2] - delta.prime[i] * step(i - nDays)) +
+              step( t[i,j,k] - delta[i,j,2] - delta.prime[i] * step(j - nDays)) +
               1
             
           ## Model response
