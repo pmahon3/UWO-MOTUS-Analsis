105a200ba6f74b5caa40ed86ffc0b8f14297faa9
Patrick Mahon <patrickmahon@ibm.com>
2021-07-22 16:18:41 -0400

    real data work

---
+++ src/main/R/Population/v1.2/Simulation Study/Model.R
@@ -0,0 +1,97 @@
+## Naming Conventions
+##
+## Indexing:
+## i -- individual birds
+## j -- day
+## k -- observation within day
+## p -- period
+##
+## Unknown parameter values:
+## muXxx -- mean of xxx. This may be compound (e.g., muMuX is the mean of muX).
+## sigmaXxx -- standard deviation of xxx. This may be compound (e.g., sigmaMuX is the SD of muX).
+## tauXxx -- precision of xxx. This is always the square of the inverse of sigmaXxx.
+##
+## Fixed parameter values: (hyperparameters)
+## etaXxx -- mean of xxx. This will be compound (e.g., etaMuX is the mean of muX when this is value is fixed).
+## dfXxx -- degrees of freedom when xxx is a standard deviation assigned a half-t prior.
+## sXxx -- scale when xxx is a standard deviation assigned a half-t prior.
+## thetaXxx -- standard deviation of xxx. 
+
+modelCode <- nimbleCode(
+  {
+    ##### Likelihood #####
+
+    ## 1) Signal strength
+
+    ## Observed values
+    for ( i in 1:nBirds ){
+      for ( j in 1:nDays ){
+        for ( k in 1:nObservations){
+          ## Identify period of day
+          p[i,j,k] <- step( t[i,j,k] - delta[i,j,1] ) +
+              step( t[i,j,k] - delta[i,j,2]) + 1
+          ## Model response
+          y[i,j,k] ~ dnorm( muY[i,j,p[i,j,k]],tauY[i,j,p[i,j,k]])
+        }
+      }
+    }
+
+      ## Period effects
+      for(p in 1:3){
+          for (bird in 1:nBirds){
+              for (day in 1:nDays){
+                  muY[bird,day,p] ~ dnorm( muMuY[p], tauMuY[p] )
+                  log(sigmaY[bird,day,p]) ~ dnorm(muSigmaY[p], tauSigmaY[p])
+                  tauY[bird, day, p] <- 1/sigmaY[bird, day, p]^2
+              }
+          }
+      }
+
+ 
+    ## 2) Changepoints
+    # Final day effect
+    for ( bird in 1:nBirds){
+      delta.prime[bird] ~ dnorm( muDelta.prime, 1/sigmaDelta.prime^2 )
+    }
+    
+    # delta, muDelta, and muMuDelta priors
+      for ( bird in 1:nBirds){
+          for(p in 1:2){
+              muDelta[bird,p] ~ dnorm(muMuDelta[p], 1/sigmaMuDelta[p]^2)
+
+              for ( day in 1:(nDays-1) ){
+                  delta[bird,day,p] ~ dnorm( muDelta[bird,p], 1/sigmaDelta[p]^2)
+              }
+          }
+          delta[bird,nDays,1] ~ dnorm(muDelta[bird,1], 1/sigmaDelta[1]^2)
+          delta[bird,nDays,2] ~ dnorm(muDelta[bird,2] + delta.prime[bird], 1/sigmaDelta[2]^2)
+      }
+    
+   ##### Priors #####
+
+      ## Changepoints
+      muDelta.prime ~ dnorm( etaMuDelta.prime, 1/thetaMuDelta.prime^2 )
+      sigmaDelta.prime ~ T(dt(0,sSigmaDelta.prime, dfSigmaDelta.prime), 0, Inf)
+
+      for(p in 1:2){
+          muMuDelta[p] ~ dnorm(etaMuMuDelta[p], 1/thetaMuMuDelta[p]^2)
+          sigmaMuDelta[p] ~ T(dt(0,sSigmaMuDelta[p], dfSigmaMuDelta[p]), 0, Inf)
+          sigmaDelta[p] ~ T(dt(0,sSigmaDelta[p], dfSigmaDelta[p]), 0, Inf)
+      }
+
+      ## Signal strength
+      for(p in 1:3){
+          ## Mean model
+          muMuY[p] ~ dnorm(etaMuMuY[p], 1 / thetaMuMuY[p]^2 )
+          sigmaMuY[p] ~ T(dt(0,sSigmaMuY[p], dfSigmaMuY[p]), 0, Inf)
+          tauMuY[p] <- 1/sigmaMuY[p]^2
+
+          ## SD model
+          muSigmaY[p] ~ dnorm(etaSigmaY[p], 1/ thetaSigmaY[p]^2)
+          sigmaSigmaY[p] ~ T(dt(0,sSigmaSigmaY[p], dfSigmaSigmaY[p]), 0, Inf)
+          tauSigmaY[p] <- 1/sigmaSigmaY[p]^2
+      }
+  }
+)
+
+
---
+++ src/main/R/Population/v1.2/Simulation Study/Test.R
@@ -0,0 +1,248 @@
+## Load packages
+library(nimble)
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(patchwork)
+
+## MCMC parameters
+thin <- 10
+niter <- 1000 #20000
+
+## Load inputs (true parameters and constants)
+source("Inputs.R")
+
+## Source simulation code
+source("Simulation.R")
+
+## Simulate data
+data <- dataSimulation(-1, constants, trueParams, FALSE)
+
+## Source model code
+source("Model.R")
+
+## Initialized nimble model
+model <- nimbleModel(code = modelCode,
+                     name = "model",
+                     constants = constants,
+                     data = data,
+                     calculate = FALSE)
+
+## Configure model
+configured <- configureMCMC(model)
+#configured$resetMonitors() #Why is this necessary?
+configured$setThin(thin)  
+
+## Set samplers (Is this necessary?)
+## configured$setSamplers(c("muDelta.prime",
+##                          "sigmaDelta.prime",
+##                          "delta.prime",
+##                          "muMuDelta",
+##                          "muDelta",
+##                          "delta",
+##                          "sigmaDelta",
+##                          "muMuY",
+##                          "sigmaMuY",
+##                          "muY",
+##                          "muSdY",
+##                          "sigmaSdY",
+##                          "sigmaY"))
+
+## Add monitors
+configured$addMonitors(c("muDelta.prime",
+                         "sigmaDelta.prime",
+                         "delta.prime",
+                         "muMuDelta",
+                         "muDelta",
+                         "delta",
+                         "sigmaDelta",
+                         "muMuY",
+                         "sigmaMuY",
+                         "muY",
+                         "muSigmaY",
+                         "sigmaSigmaY",
+                         "sigmaY"))
+
+## Build model
+built <- buildMCMC(configured)
+
+## Compile model
+compiled <- compileNimble( model, built, showCompilerOutput = TRUE )
+
+## Run model
+system.time(compiled$built$run(niter = niter))
+  
+## Extract samples
+samples <- as.matrix(compiled$built$mvSamples)
+
+samplesdf <- samples %>%
+    as_tibble() %>%
+    rowid_to_column(var = "Iteration") %>%
+    gather(key = "Parameter", value = "Value", -Iteration)
+
+## Examine results
+
+## 1) muDelta.prime and sigmaDelta.prime
+plot1 <- samplesdf %>%
+    filter(Parameter == "muDelta.prime") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muDelta.prime, colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "sigmaDelta.prime") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaDelta.prime, colour = "red", lty = 2)
+
+plot1 / plot2
+
+## 2) muMuDelta
+plot1 <- samplesdf %>%
+    filter(Parameter == "muMuDelta[1]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muMuDelta[1], colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "muMuDelta[2]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muMuDelta[2], colour = "red", lty = 2)
+
+plot1 / plot2
+
+## 3) sigmaMuDelta
+plot1 <- samplesdf %>%
+    filter(Parameter == "sigmaMuDelta[1]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaMuDelta[1], colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "sigmaMuDelta[2]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaMuDelta[2], colour = "red", lty = 2)
+
+plot1 / plot2
+
+## 4) sigmaDelta
+plot1 <- samplesdf %>%
+    filter(Parameter == "sigmaDelta[1]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaDelta[1], colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "sigmaDelta[2]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaDelta[2], colour = "red", lty = 2)
+
+plot1 / plot2
+
+## 4) muMuY
+plot1 <- samplesdf %>%
+    filter(Parameter == "muMuY[1]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muMuY[1], colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "muMuY[2]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muMuY[2], colour = "red", lty = 2)
+
+plot3 <- samplesdf %>%
+    filter(Parameter == "muMuY[3]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muMuY[3], colour = "red", lty = 2)
+
+plot1 / plot2 / plot3
+
+
+## 5) sigmaMuY
+plot1 <- samplesdf %>%
+    filter(Parameter == "sigmaMuY[1]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaMuY[1], colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "sigmaMuY[2]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaMuY[2], colour = "red", lty = 2)
+
+plot3 <- samplesdf %>%
+    filter(Parameter == "sigmaMuY[3]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaMuY[3], colour = "red", lty = 2)
+
+plot1 / plot2 / plot3
+
+## 6) muSdY
+plot1 <- samplesdf %>%
+    filter(Parameter == "muSigmaY[1]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muSigmaY[1], colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "muSigmaY[2]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muSigmaY[2], colour = "red", lty = 2)
+
+plot3 <- samplesdf %>%
+    filter(Parameter == "muSigmaY[3]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$muSigmaY[3], colour = "red", lty = 2)
+
+plot1 / plot2 / plot3
+
+## 6) sdSigmaY
+plot1 <- samplesdf %>%
+    filter(Parameter == "sigmaSigmaY[1]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaSigmaY[1], colour = "red", lty = 2)
+
+plot2 <- samplesdf %>%
+    filter(Parameter == "sigmaSigmaY[2]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaSigmaY[2], colour = "red", lty = 2)
+
+plot3 <- samplesdf %>%
+    filter(Parameter == "sigmaSigmaY[3]") %>%
+    ggplot(aes(x = Iteration, y = Value)) +
+    geom_line() +
+    geom_hline(yintercept = trueParams$sigmaSigmaY[3], colour = "red", lty = 2)
+
+plot1 / plot2 / plot3
+
+## 7) Delta.prime
+samplesdf %>%
+    filter(grepl("delta.prime\\[",Parameter)) %>%
+    group_by(Parameter) %>%
+    summarize(
+        Mean = mean(Value),
+        Lower = quantile(Value, .025),
+        Upper = quantile(Value, .975))
+
+## 8) delta[1]
+samplesdf %>%
+    filter(grepl("delta\\[([0-9]), 15, 2\\]",Parameter)) %>%
+    group_by(Parameter) %>%
+    summarize(
+        Mean = mean(Value),
+        Lower = quantile(Value, .025),
+        Upper = quantile(Value, .975))
+
